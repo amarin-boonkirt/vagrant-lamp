71043da3465f7c614bd167ec9dc872496b992c2b
Amarin Boonkirt <amarin-boonkirt@users.noreply.github.com>
2016-12-16 23:01:54 +0700

    Update provision.sh
    fix false commit

--- provision.sh
+++ provision.sh
@@ -1,32 +1,163 @@
-# -*- mode: ruby -*-
-# vi: set ft=ruby :
+#!/bin/bash
 
-# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
-VAGRANTFILE_API_VERSION = "2"
+apache_config_file="/etc/apache2/envvars"
+apache_vhost_file="/etc/apache2/sites-available/vagrant_vhost.conf"
+php_config_file="/etc/php5/apache2/php.ini"
+xdebug_config_file="/etc/php5/mods-available/xdebug.ini"
+mysql_config_file="/etc/mysql/my.cnf"
+default_apache_index="/var/www/html/index.html"
+project_web_root="/var/www/html"
 
-Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-    config.vm.box = "ubuntu/trusty64"
+# This function is called at the very bottom of the file
+main() {
+	repositories_go
+	update_go
+	network_go
+	tools_go
+	apache_go
+	mysql_go
+	php_go
+	phpcms_go
+	autoremove_go
+}
 
-    config.vm.network :private_network, ip: "192.168.33.10"
-    config.vm.synced_folder ".", "/vagrant", mount_options: ['dmode=777','fmode=666']
+repositories_go() {
+	"cp" -f /vagrant/src/etc/apt/sources.list /etc/apt/sources.list
 
-    config.vm.synced_folder "../../../works", "/var/www/html/works",
-      owner: "www-data",
-      group: "www-data",
-      mount_options: ["dmode=755,fmode=644"]
+	cp -f /vagrant/src/index.php /var/www/html/
+}
 
- #    config.vm.synced_folder "../../plugins/wpkit", "/var/www/html/wordpress/wp-content/plugins/wpkit",
- #     owner: "www-data",
- #     group: "www-data",
- #     mount_options: ["dmode=755,fmode=644"]#
+update_go() {
+	# Update the server
+	apt-get update
+	# apt-get -y upgrade
+}
 
-	config.vm.provision "shell", path: "provision.sh"
+autoremove_go() {
+	apt-get -y autoremove
+}
 
-  # config.vm.provider "virtualbox" do |vb|
-  #   # Don't boot with headless mode
-  #   vb.gui = true
-  #
-  #   # Use VBoxManage to customize the VM. For example to change memory:
-  #   vb.customize ["modifyvm", :id, "--memory", "1024"]
-  # end
-end
+network_go() {
+	IPADDR=$(/sbin/ifconfig eth0 | awk '/inet / { print $2 }' | sed 's/addr://')
+	sed -i "s/^${IPADDR}.*//" /etc/hosts
+	echo ${IPADDR} ubuntu.localhost >> /etc/hosts			# Just to quiet down some error messages
+}
+
+tools_go() {
+	# Install basic tools
+	apt-get -y install build-essential binutils-doc git subversion
+}
+
+apache_go() {
+	# Install Apache
+	apt-get -y install apache2
+	# suphp-common libapache2-mod-suphp
+
+	#sed -i "s/umask=0077/umask=0022/g" /etc/suphp/suphp.conf
+
+	if [ ! -f "${apache_vhost_file}" ]; then
+		cat << EOF > ${apache_vhost_file}
+<VirtualHost *:80>
+    ServerAdmin webmaster@localhost
+    DocumentRoot ${project_web_root}
+    LogLevel debug
+
+    #suPHP_Engine on
+
+    ErrorLog /var/log/apache2/error.log
+    CustomLog /var/log/apache2/access.log combined
+
+    <Directory ${project_web_root}>
+        AllowOverride All
+        Require all granted
+        DirectoryIndex index.php
+        DirectoryIndex index.html
+    </Directory>
+</VirtualHost>
+EOF
+	fi
+
+	a2dissite 000-default
+	a2ensite vagrant_vhost
+
+	a2enmod rewrite
+
+	service apache2 reload
+	update-rc.d apache2 enable
+}
+
+php_go() {
+	apt-get -y install php5 php5-curl php5-mysql php5-sqlite php5-xdebug php-pear
+
+	sed -i "s/display_startup_errors = Off/display_startup_errors = On/g" ${php_config_file}
+	sed -i "s/display_errors = Off/display_errors = On/g" ${php_config_file}
+
+	if [ ! -f "{$xdebug_config_file}" ]; then
+		cat << EOF > ${xdebug_config_file}
+zend_extension=xdebug.so
+xdebug.remote_enable=1
+xdebug.remote_connect_back=1
+xdebug.remote_port=9000
+xdebug.remote_host=10.0.2.2
+EOF
+	fi
+
+	service apache2 reload
+
+	# Install latest version of Composer globally
+	if [ ! -f "/usr/local/bin/composer" ]; then
+		curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+	fi
+
+	# Install PHP Unit 4.8 globally
+	if [ ! -f "/usr/local/bin/phpunit" ]; then
+		curl -O -L https://phar.phpunit.de/phpunit-old.phar
+		chmod +x phpunit-old.phar
+		mv phpunit-old.phar /usr/local/bin/phpunit
+	fi
+}
+
+phpcms_go() {
+    cd /usr/src
+    wget https://files.phpmyadmin.net/phpMyAdmin/4.6.5.2/phpMyAdmin-4.6.5.2-english.tar.gz
+    tar -xvf phpMyAdmin-4.6.5.2-english.tar.gz
+    mv phpMyAdmin-4.6.5.2-english /var/www/html/phpmyadmin
+
+    wget https://wordpress.org/latest.tar.gz
+    tar -xvf latest.tar.gz
+    mv wordpress /var/www/html/wordpress
+	chown www-data:www-data -R /var/www/html/wordpress
+    rm -f phpMyAdmin-4.6.5.2-english.tar.gz
+    rm -f latest.tar.gz
+}
+
+mysql_go() {
+	# Install MySQL
+	echo "mysql-server mysql-server/root_password password root" | debconf-set-selections
+	echo "mysql-server mysql-server/root_password_again password root" | debconf-set-selections
+	apt-get -y install mysql-client mysql-server
+
+	sed -i "s/bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" ${mysql_config_file}
+
+	# Allow root access from any host
+	echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION" | mysql -u root --password=root
+	echo "GRANT PROXY ON ''@'' TO 'root'@'%' WITH GRANT OPTION" | mysql -u root --password=root
+
+	if [ -d "/vagrant/provision-sql" ]; then
+		echo "Executing all SQL files in /vagrant/provision-sql folder ..."
+		echo "-------------------------------------"
+		for sql_file in /vagrant/provision-sql/*.sql
+		do
+			echo "EXECUTING $sql_file..."
+	  		time mysql -u root --password=root < $sql_file
+	  		echo "FINISHED $sql_file"
+	  		echo ""
+		done
+	fi
+
+	service mysql restart
+	update-rc.d apache2 enable
+}
+
+main
+exit 0
