36cae3f7652c1a695b0ad16a6f274c50689f1c87
Amarin Boonkirt <amarin-boonkirt@users.noreply.github.com>
2016-12-16 21:31:16 +0700

    Update index.php
    Add phpmyadmin,wordpress link

--- src/index.php
+++ src/index.php
@@ -2,6 +2,10 @@
 <html lang="en">
 	<body>
 		<h1>Welcome to Yet Another LAMP Server!</h1>
+		<a href="http://local.dev/phpmyadmin/" target="_blank">PHPMyAdmin</a> |
+		<a href="http://local.dev/wordpress/" target="_blank">Wordpress Themes-Plugins developement/</a> |
+		<a href="http://local.dev/works/" target="_blank">Workspace/</a>
+
 		<?php
 			phpinfo();
 		?>
